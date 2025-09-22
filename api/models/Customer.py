63726694ed8174073d9941e5beedcf0646c657c0
Ville <ville.kaikk@gmail.com>
2025-09-22 19:46:04 +0300

    Fix model definitions

--- api/models/Customer.py
+++ api/models/Customer.py
@@ -1,17 +1,17 @@
 from datetime import datetime
+from typing import TYPE_CHECKING, Optional
 
-from pydantic import BaseModel
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+
+if TYPE_CHECKING:
+    from models.User import User
+    from models.Message import Message
 
 
 class Customer(SQLModel, table=True):
-    id : int | None  = Field(default=None, primary_key=True)
+    id : int | None  = Field(default=None, primary_key=True, index=True)
     name: str = Field(nullable=False)
     contact_id: int = Field(foreign_key="user.id")
-    created_at : datetime = Field(nullable=False)
-
-
-class CustomerCreate(BaseModel):
-    name: str
-    contact_id: int
-    created_at: datetime
+    contact: Optional["User"] = Relationship(back_populates="customers")
+    messages: list["Message"] = Relationship(back_populates="customer")
+    created_at: Optional[datetime] = Field(default_factory=lambda: datetime.now())
--- api/models/User.py
+++ api/models/User.py
@@ -1,13 +1,14 @@
-from pydantic import BaseModel
-from sqlmodel import SQLModel, Field
+from typing import Optional
+
+from sqlmodel import SQLModel, Field, Relationship
+
+from models.Message import Message
+from models.Customer import Customer
 
 
 class User(SQLModel, table=True):
-    id : int | None  = Field(default=None, primary_key=True)
-    name: str = Field(nullable=False)
-    email : str = Field(nullable=False)
-
-
-class UserCreate(BaseModel):
-    name: str
-    email: str
+    id: Optional[int]  = Field(default=None, primary_key=True, index=True)
+    name: str = Field(nullable=False, index=True)
+    email: str = Field(nullable=False)
+    customers: list["Customer"] = Relationship(back_populates="contact")
+    messages: list["Message"] = Relationship(back_populates="user")
